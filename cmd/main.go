f2325cbae1b3f27fadaa41ce0ba0b81e7ee599fc
shkar <shkarhsardar@gmail.com>
2024-01-12 22:09:42 +0300

    reset

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"gogit/internal"
 	"log"
 
 	"github.com/spf13/cobra"
 )
-
-// var args struct {
-// 	Init       *internal.InitCmd       `arg:"subcommand:init"`
-// 	HashObject *internal.HashObjectCmd `arg:"subcommand:hash-object"`
-// 	HashCat    *internal.HashCatCmd    `arg:"subcommand:cat"`
-// 	WriteTree  *internal.WriteTreeCmd  `arg:"subcommand:write-tree"`
-// }
 
 var rootCmd = &cobra.Command{
 	Use:   "Gogit",
@@ -26,7 +18,7 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize repoistory",
 	Run: func(cmd *cobra.Command, args []string) {
-		internal.InitializeRepository()
+
 	},
 }
 
@@ -36,27 +28,4 @@ func main() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	// p := arg.MustParse(&args)
-	// if p.Subcommand() == nil {
-	// 	p.WriteHelp(os.Stderr)
-	// 	return
-	// }
-	// if args.Init != nil {
-	// 	internal.InitializeRepository()
-	// 	return
-	// }
-	// switch {
-	// case args.HashObject != nil:
-	// 	{
-	// 		internal.CreateBlobHashObject(args.HashObject)
-	// 	}
-	// case args.HashCat != nil:
-	// 	{
-	// 		print(internal.HashCat(args.HashCat, internal.ObjTag(args.HashCat.Type)))
-	// 	}
-	// case args.WriteTree != nil:
-	// 	{
-	// 		internal.WriteTree()
-	// 	}
-	// }
 }
